77eeb27c33b0c9251fc738fd170fe81d74dfebf3
Francesco Ferraro <39172206+FerraroF@users.noreply.github.com>
2019-04-07 12:20:23 +0200

    Create list.py

---
+++ list.py
@@ -0,0 +1,37 @@
+## PRIMO ANNO
+# Analisi 1                 vittone
+# Sperimentazioni 1         sada, mazzocco, mengoni
+# Chimica                   vittadini
+# geometria                 balda(ssarri), garuti, urbinati??
+#                           quel tutor russo???
+# fisica 1                  carnera, lacaprara
+# analisi 2                 monti
+
+## SECONDO ANNO
+# fisica 2                  zwirner, zanetti
+# analisi 3                 marastoni
+# speri 2                   bastieri, cocola, patelli, salasnich, michieli
+# fassò                     fassò
+# moderna                   marchetti
+# metodi mat                martucci
+
+## TERZO ANNO
+# teorica                   feruglio, stella
+# campi                     giusto
+# speri 3                   agarfa, lunardon, enrico
+# metodi comp               trovato
+# materia                   .........
+# nucsubnuc                 fortunato, lucchesi
+# gravitazione              zendri
+
+## VARI
+# meccanica analitica       benettin
+# geodiff                   bottacin
+# pianeti                   marzari
+# plasmi                    serianni
+# termodinamica             pierno, ????
+# fluidodinamica            mistura
+
+## EVENTUALI
+# BETTINI
+# PERUZZI
