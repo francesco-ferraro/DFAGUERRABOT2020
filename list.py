823ed59ddc5cc9fc7e23888ff4266b3d1aa0bd29
ombromanto <42914369+aleable@users.noreply.github.com>
2019-04-09 13:48:01 +0200

    Update list.py

--- list.py
+++ list.py
@@ -1,46 +1,4 @@
-## Primo piano
-# Carnera       117         nell'atrio
-# Enrico (amico di garfa)
-# Garfagnini    164         ala centrale? comunque vicino enrico
-#                           poster cheguevara, era suo?
-# Lacaprara     137
-# Trovato                   corridoio a destra dell'entrata TODO che ala?
-# Zanetti       132         corridoio a sinistra dell'entrata
-
-## Secondo piano 
-# Marchetti     242         mi pare ala centrale
-      
-## Terzo piano
-# Borghesani                ala est, davanti torre
-# Feruglio      313
-# Fortunato     353         davanti alle scale centrali
-# Giusto        355         davanti alle scale centrali
-# Martucci      357         
-# Mengoni       322
-# Stella        332         angolo sud-ovest
-# Zwirner       314
-# Zendri        389         sopra la rostagni
-
-# Altro
-# Mazzocco (paolotti)
-# Lucchesi (paolotti)
-# Sada ?
-# Lunardon
-# Vittadini ?
-# Vittone
-# Baldassarri, Garuti, Urbinati
-# Monti
-# Marastoni
-# Fassò
-# Benettin
-# Bottacin
-
-# Sperimentazioni 2     bastieri, cocola, patelli, salasnich, michieli
-# pianeti                   marzari
-# plasmi                    serianni
-# termodinamica             pierno, ????
-# fluidodinamica            mistura
-
+amica            mistura
 # BETTINI
 # PERUZZI
 # BAIESI
@@ -59,3 +17,13 @@
 # Rigolin
 # STANCO
 # ROBERTO (tecnico rostagni)
+
+Aggiungerei
+Maritan
+Lechner
+Uomo Civis
+Carmela (la bibliotecaria)
+Peruzzi (che non ho messo)
+Roberto (tecnico della Rostagni)
+Raffaella Cesaro
+Pacciani
