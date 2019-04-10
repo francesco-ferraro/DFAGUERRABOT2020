90eabf42338b36c9c4e0f5d9c63faa4e34b47073
ombromanto <42914369+aleable@users.noreply.github.com>
2019-04-10 20:17:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import map as map
+
+'''
+inizializza la mappa: genera i colori degli uffici a random
+ritorna i colori degli uffici [[RGB1_uff1, RGB2_uff1, RGB3_uff1]...[RGB1_uff48, RGB2_uff48, RGB3_uff48]]
+'''
+color = map.initialplot()
+'''
+updatecolor(ufficio sconfitto, ufficio vincitore, color)
+ritorna il vettore dei colori aggironato dopo la battaglia
+'''
+colorup = map.updatecolor(2, 1, color)
+map.replot(colorup)
