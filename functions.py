901e23ba2ba7587e4d4374834fbb9d31f904c29a
ombromanto <42914369+aleable@users.noreply.github.com>
2019-04-11 22:59:36 +0200

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,16 @@
+import map as map
+
+'''
+inizializza la mappa: genera i colori degli uffici a random
+ritorna i colori degli uffici [[RGB1_uff1, RGB2_uff1, RGB3_uff1]...[RGB1_uff48, RGB2_uff48, RGB3_uff48]]
+'''
+(color, vettoreperfra) = map.initialplot()
+
+'''
+updatecolor(ufficio sconfitto, ufficio vincitore, color)
+ritorna il vettore dei colori aggiornato dopo la battaglia e il vettore che ti serve
+'''
+#NB: il primo input di map.replot deve essere sempre il vettore color output di map.initialplot()
+colorupdated = map.updatecolor(1, 0, color)
+(colornew, vettoreperfradopolaguerra) = map.replot(color, colorupdated)
+ 
